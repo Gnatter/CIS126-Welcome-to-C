00e4ea4644575f37325226fe7dec140f54655b67
Gnatter <snagg23@live.com>
2019-08-25 21:12:14 -0400

    My initial commit

---
+++ Welcome to C/Welcome to C.cpp
@@ -0,0 +1,12 @@
+// Welcome to C.cpp : Defines the entry point for the console application.
+// First program for CIS126 by Shawn O'Lear
+
+#include <StdAfx.h>	// required to aviod fatal error C1010 during compiling
+#include <stdio.h>	// standard input output header
+
+// function main begins program execution
+int main(void)
+{
+	printf("Welcome to C!\n");	// displays Welcome to C! to the output
+	return 0;	// indicates program ended successfully
+} // end function
